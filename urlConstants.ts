79e1b047ebc903000a728adae2b825275aea7bdb
sathishsachu1995 <154673827+sathishsachu1995@users.noreply.github.com>
2025-10-15 10:12:18 +0530

    Add files via upload

---
+++ urlConstants.ts
@@ -0,0 +1,5 @@
+export enum UrlConstants{
+    preliveExportPageUrl = "https://prelive-au-customer.worldoptions.com/#/Dashboard/quote",
+    liveExportPageUrl = "https://au-customer.worldoptions.com/#/Dashboard/export",
+    shopifyUrl = "https://admin.shopify.com/store/test-au-wo"
+}
